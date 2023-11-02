a2d91cfc665da45bb1b7884a9b3bf412fd762782
Cami Fresch <cami.fresch@hotmail.com>
2023-11-02 00:53:13 -0300

    sec commit hb

--- desafioentregableM1-10/src/app.js
+++ desafioentregableM1-10/src/app.js
@@ -2,17 +2,27 @@ import cartRouter from './routes/cart.router.js';
 import productsRouter from './routes/products.router.js';
 import express from "express";
 import { __dirname } from './utils.js';
+import { engine } from 'express-handlebars';
+import ProductManager from './ProductManager.js';
+import { randomNumber } from './utils.js';
 
 const PUERTO = 8080
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
-
 app.use("/", express.static(__dirname + "/public"))
-
 app.use('/api', productsRouter, cartRouter)
 
+app.engine("handlebars", engine())
+app.set('views', `${__dirname}/views`);
+app.set("view engine", "handlebars")
+
+app.get('/', (req, res) => {
+    const position = randomNumber(0, ProductManager.length - 1);
+    const products = ProductManager[position];
+    res.render('index', { title: 'G Store👙', ...products});
+  });
 
 app.listen(PUERTO, () => {
     console.log(`Servidor express activo en puerto ${PUERTO}`);
--- desafioentregableM1-10/src/utils.js
+++ desafioentregableM1-10/src/utils.js
@@ -1,6 +1,8 @@
 import * as url from 'url';
 
+const randomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
-export { __filename, __dirname};
+export { __filename, __dirname, randomNumber};
